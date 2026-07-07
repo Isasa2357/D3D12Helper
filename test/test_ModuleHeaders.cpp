87415c57f0dbecd9aa726b10d3c04b0c63dfb0a9
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-07 17:07:07 +0900

    Add module header smoke test

---
+++ test/test_ModuleHeaders.cpp
@@ -0,0 +1,13 @@
+#include "TestFramework.hpp"
+
+#include <D3D12Helper/D3D12Foundation/D3D12Foundation.hpp>
+#include <D3D12Helper/D3D12Core/D3D12Core.hpp>
+#include <D3D12Helper/D3D12Gpu/D3D12Gpu.hpp>
+#include <D3D12Helper/D3D12Presentation/D3D12Presentation.hpp>
+#include <D3D12Helper/D3D12Processing/D3D12Processing.hpp>
+#include <D3D12Helper/D3D12Interop/D3D12Interop.hpp>
+#include <D3D12Helper/D3D12Framework/D3D12Framework.hpp>
+
+TEST(ModuleHeaders, UmbrellaIncludes) {
+    CHECK(true);
+}
