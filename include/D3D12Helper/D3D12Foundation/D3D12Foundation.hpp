e10d57b33f658cc9e2917ab447fe5609f89a3781
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-07 17:04:26 +0900

    Add D3D12Foundation umbrella header

---
+++ include/D3D12Helper/D3D12Foundation/D3D12Foundation.hpp
@@ -0,0 +1,5 @@
+#pragma once
+#include <D3D12Helper/D3D12Foundation/D3D12Common.hpp>
+#include <D3D12Helper/D3D12Foundation/ThrowIfFailed.hpp>
+#include <D3D12Helper/D3D12Foundation/DxgiUtil.hpp>
+#include <D3D12Helper/D3D12Foundation/D3D12FormatUtil.hpp>
